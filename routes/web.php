76d95c83f65d6873757da1f6f90ee995c132ac34
sryya krby <sureyyakarabay@gmail.com>
2026-05-23 10:36:05 +0300

    login işlemi eklendi

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function (): void {
+    Route::redirect('/login', '/salih/login')->name('login');
     Route::get('/register', [RegisterController::class, 'create'])->name('register');
     Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 });
